27d111d1116a0d222d0b663f1ded86e518970a12
fabioass1s <fabio.junior59@hotmail.com>
2025-07-09 13:15:22 -0300

    retirada de lixo

--- src/adapters/controllers/routes/models/routes_dto.py
+++ src/adapters/controllers/routes/models/routes_dto.py
@@ -10,16 +10,5 @@ class RoutesDTO(BaseModel):
     longitude_destino: str = Field(..., example="-49.234030")
 
 
-class InsertLocsDTO(BaseModel):
-    nome_localizacao: str = Field(..., example="Posto do fulano")
-    tipo: str = Field(..., example="Posto de combustíveis")
-    latitude: str = Field(..., example="-16.804450")
-    longitude: str = Field(..., example="-49.205938")
 
-class UpdloadDfLocsDTO(BaseModel):
-    coluna_tipo_localizacao: str = Field(..., example="tipo_localizacao")
-    coluna_nome_localizacao: Optional[str] = Field(None, example="nome_localizacao")
-    coluna_latitude: str = Field(..., example="latitude")
-    coluna_longitude: str = Field(..., example="longitude")
-    separador_csv: Optional[str] = Field(None, example=",", description="Separador do CSV, padrão é vírgula (',')")
     
